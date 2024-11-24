213a5046f2baf98e9c43fde335b777c92f43b072
Mateusz <149837316+maryszawy@users.noreply.github.com>
2024-11-24 19:40:51 +0100

    Add files via upload

---
+++ Library_server/dockerTest002/Models/ConfConnDB.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dockerTest002.Models
+{
+    public class ConfConnDB : DbContext
+    {
+        public ConfConnDB(DbContextOptions<ConfConnDB> options) : base(options) { }
+        public DbSet<Ksiazka> Ksiazka { get; set; }
+    }
+}
---
+++ Library_server/dockerTest002/Models/Ksiazka.cs
@@ -0,0 +1,11 @@
+﻿namespace dockerTest002.Models
+{
+    public class Ksiazka
+    {
+        public int KsiazkaID { get; set; }
+        public string Tytul { get; set; }
+        public string Autor { get; set; }
+        public bool Dostepnosc { get; set; }
+        public string Gatunek { get; set; }
+    }
+}
