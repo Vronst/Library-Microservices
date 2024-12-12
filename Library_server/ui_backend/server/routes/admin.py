26bca5aea4841dfdb27f0807c04132485a0cb027
Adam <adam.p.sarga@gmail.com>
2024-12-12 13:50:30 +0100

    checkpoint

--- Library_server/ui_backend/server/routes/admin.py
+++ Library_server/ui_backend/server/routes/admin.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, Optional
+from typing import Any, Callable, Optional, Type
 from flask import Blueprint, render_template, request, redirect, url_for
 from werkzeug.wrappers.response import Response
 from flask_login import login_required
@@ -14,8 +14,19 @@ from ..forms.forms_admin import (
 from .. import session as db_session
 from ..utils import is_admin
 
-# TODO: Admin page whith option to add things
 
+stf: dict[str, Type[FlaskForm]] = {
+    'User': FormUser,
+    'Library': FormLibrary,
+    'RecentRead': FormRecent,
+    'Family': FormFamily
+}
+stm: dict[str, Type[Base]] = {
+    'User': User,
+    'Library': Library,
+    'RecentRead': RecentRead,
+    'Family': Family,
+}
 admin = Blueprint('admin',
                   __name__,
                   template_folder=os.path.join(
@@ -45,33 +56,26 @@ def index() -> str:
 @is_admin
 def edit_db(db: str, id: Optional[int] = None) -> str | Response | tuple[str, int]:
     data: list[Any]
-    stm: dict = {
-        'User': User,
-        'Library': Library,
-        'RecentRead': RecentRead,
-        'Family': Family,
-    }
-    query = lambda x: db_session.query(stm[x]).all()
+    query: Callable = lambda x: db_session.query(stm[x]).all()
     data = query(db)        
-    columns = [column.key for column in stm[db].__table__.columns
+    columns: list[str] = [column.key for column in stm[db].__table__.columns
             if column.key not in ['password', 'id']]
-    return render_template('db_view.html', data=data, columns=columns)
+    return render_template('db_view.html', data=data, columns=columns, db=db)
 
 
 # TODO: Finish this and start matis form !!!
 @admin.route('/db/<string:db>/<int:id>', methods=['POST', 'DELETE', 'PUT', 'PATCH'])
 @login_required
 @is_admin
-def edit_form(db: str, id: int) -> str | Response:
-    if request.method in ['POST', 'PUT', 'PATCH']:
-        stf: dict[str, FlaskForm] = {
-            'User': FormUser,
-            'Library': FormLibrary,
-            'RecentRead': FormRecent,
-            'Family': FormFamily
-        }
-        form: FlaskForm = stf[db](obj=) if not request.method == 'POST' else stf[db]()
-        return redirect(url_for('admin.index'))
+def edit_form(db: str, id: int) -> str | tuple[str, int]:
+    # Yes i know it should be id_ or smth else then id..
+    if request.method in ['PUT', 'PATCH', 'POST']:
+        obj: Base | None = db_session.query(stm[db]).get(ident=id)
+        post_check = request.form['_method'] if '_method' in request.form.keys() else False
+        form: FlaskForm = stf[db](obj=obj) if not request.method == 'POST' or post_check == 'PUT' else stf[db]()
+        columns: list[str] = [column.key for column in stm[db].__table__.columns
+                if column.key not in ['password', 'id']]
+        return render_template('edit_form.html', form=form, columns=columns)
     elif request.method == 'DELETE':
         ...
         return '', 204
